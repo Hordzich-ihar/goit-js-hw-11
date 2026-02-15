afb78ff200e655817f6fe9f447fd8d12d7bc3254
hordzich ihar <Garik666770@gmail.com>
2026-02-15 19:19:27 +0300

    new co

--- js/pixabay-api.js
+++ js/pixabay-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '54645624-93bc72701cdff138f1edf2876';
+const API_KEY = import.meta.env.VITE_PIXABAY_KEY;
 const BASE_URL = 'https://pixabay.com/api/';
 
 const httpClient = axios.create({
@@ -18,7 +18,9 @@ export const hasApiKey = Boolean(API_KEY);
 
 export function getImagesByQuery(query) {
   if (!API_KEY) {
-    return Promise.reject(new Error('Pixabay API key is missing in the app code.'));
+    return Promise.reject(
+      new Error('Pixabay API key is missing. Set VITE_PIXABAY_KEY in your .env file.')
+    );
   }
 
   return httpClient.get('', { params: { q: query } }).then(({ data }) => data);
