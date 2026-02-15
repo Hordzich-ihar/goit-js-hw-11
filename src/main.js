938ebe9dbb402dd008c94ce35506e7632740b73e
hordzich ihar <Garik666770@gmail.com>
2026-02-15 22:00:24 +0300

    new code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import { getImagesByQuery, hasApiKey } from '../js/pixabay-api.js';
+import { getImagesByQuery, hasApiKey } from './js/pixabay-api.js';
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
-} from '../js/render-functions.js';
+} from './js/render-functions.js';
 
 const form = document.querySelector('.form');
 const searchInput = document.querySelector('input[name="search-text"]');
